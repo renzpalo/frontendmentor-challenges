471ee799a7e0b1a5f2b486763e8ed4d3ef02bb4d
Renz Palo <renzpalo@gmail.com>
2020-06-21 22:55:55 -0700

    Added rest of contents & design

--- easybank-landing-page/js/scripts.js
+++ easybank-landing-page/js/scripts.js
@@ -2,4 +2,21 @@
 
 // Navigation Menu
 const btnNavMenu = document.querySelector('.nav-menu-icon');
+const navMenu = document.querySelector('.nav-menu-list');
 
+btnNavMenu.addEventListener('click', () => {
+    // Open/close navigation menu
+    if(navMenu.style.display == 'none' || navMenu.style.display == '') {
+        btnNavMenu.classList.remove('nav-open');
+        btnNavMenu.classList.add('nav-close');
+
+        navMenu.style.display = 'block';
+    } else {
+        console.log('close');
+
+        btnNavMenu.classList.remove('nav-close');
+        btnNavMenu.classList.add('nav-open');
+
+        navMenu.style.display = 'none';
+    }
+});
