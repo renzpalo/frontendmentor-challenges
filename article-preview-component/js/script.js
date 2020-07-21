31caba799d02d04112d31c7a04ae0518f38c6fdb
Renz Palo <renzpalo@gmail.com>
2020-07-20 22:01:49 -0700

    Media query fix

--- article-preview-component/js/script.js
+++ article-preview-component/js/script.js
@@ -7,14 +7,14 @@ const cardShareLinks = document.querySelector('.card-share-links');
 const shareButton = document.querySelector('.card-share-button');
 
 
-shareButton.addEventListener('mouseover', () => {
-    cardAuthor.style.backgroundColor = 'hsl(217, 19%, 35%)';
-    cardAuthorTexts.style.display = 'none';
-    cardShareLinks.style.display = 'flex';
-});
-
-shareButton.addEventListener('mouseout', () => {
-    cardAuthor.style.backgroundColor = '#FFF';
-    cardAuthorTexts.style.display = 'flex';
-    cardShareLinks.style.display = 'none';
+shareButton.addEventListener('click', () => {
+    if(cardAuthorTexts.style.display == 'flex' || cardAuthorTexts.style.display == '') {
+        cardAuthor.style.backgroundColor = 'hsl(217, 19%, 35%)';
+        cardAuthorTexts.style.display = 'none';
+        cardShareLinks.style.display = 'flex';
+    } else if(cardAuthorTexts.style.display == 'none') {
+        cardAuthor.style.backgroundColor = '#FFF';
+        cardAuthorTexts.style.display = 'flex';
+        cardShareLinks.style.display = 'none';
+    }
 });
