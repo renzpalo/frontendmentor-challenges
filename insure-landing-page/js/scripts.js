07ae7d722ad0c77cee149c8f1426fbbd69a3a10e
Renz Palo <renzpalo@gmail.com>
2022-04-17 13:45:28 +0800

    added script for hamburger menu

---
+++ insure-landing-page/js/scripts.js
@@ -0,0 +1,20 @@
+
+
+const navHamburger = document.querySelector('.nav-hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+navHamburger.addEventListener('click', (e) => {
+    console.log('click');
+
+    console.log()
+
+    if(navMenu.style.display == '' || navMenu.style.display == null || navMenu.style.display == 'none') {
+        navMenu.style.display = 'flex';
+        navHamburger.children[0].src = 'images/icon-close.svg';
+    } else {
+        navMenu.style.display = 'none';
+        navHamburger.children[0].src = 'images/icon-hamburger.svg';
+    }
+
+    
+});
