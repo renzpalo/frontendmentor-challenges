004cafe87b5aacf277c64339b5e21b296ecddc6b
Renz Palo <renzpalo@gmail.com>
2022-05-09 19:01:20 +0800

    added scripts;

--- interactive-pricing-component/js/script.js
+++ interactive-pricing-component/js/script.js
@@ -1,9 +1,38 @@
 
 const optionsRange = document.getElementById('pricing-options__range');
 const optionsPriceValue = document.querySelector('.pricing-options__price-value');
+const optionsPageviewValue = document.querySelector('.interactive_pricing__pageview-value');
+
+const optionsToggle = document.getElementById('billing-options__toggle');
 
 optionsRange.addEventListener('change', (event) => {
-  console.log(event.target.value);
+  let priceValue = parseInt(event.target.value);
+  let pageViewsValue = '';
 
-  optionsPriceValue.textContent = `$${parseFloat(event.target.value).toFixed(2)}`;
+  if(priceValue <= 8) {
+    pageViewsValue = '10K';
+  } else if(priceValue > 8 && priceValue <= 12) {
+    pageViewsValue = '50K';
+  } else if(priceValue > 12 && priceValue <= 16) {
+    pageViewsValue = '100K';
+  } else if(priceValue > 16 && priceValue <= 24) {
+    pageViewsValue = '500K';
+  } else if(priceValue > 24 && priceValue <= 36) {
+    pageViewsValue = '1M';
+  }
+
+  optionsPriceValue.textContent = `$${parseFloat(priceValue).toFixed(2)}`;
+  optionsPageviewValue.textContent = pageViewsValue;
+});
+
+optionsToggle.addEventListener('change', (event) => {
+  let optionsRangeValue = optionsRange.value;
+  if(event.target.checked) {
+    optionsRangeValue = optionsRangeValue * 0.25;
+    
+  } else {
+    optionsRangeValue = optionsRangeValue;
+  }
+
+  optionsPriceValue.textContent = `$${parseFloat(optionsRangeValue).toFixed(2)}`;
 });
