42376b44767fc44b4069f685843241c285a3a8e9
Renz Palo <renzpalo@gmail.com>
2022-05-12 13:33:38 +0800

    toggle switch for discount

--- interactive-pricing-component/js/script.js
+++ interactive-pricing-component/js/script.js
@@ -6,19 +6,29 @@ const optionsPageviewValue = document.querySelector('.interactive_pricing__pagev
 const optionsToggle = document.getElementById('billing-options__toggle');
 
 optionsRange.addEventListener('change', (event) => {
-  let priceValue = parseInt(event.target.value);
+  let priceValue = parseInt(event.target.value) / 1000;
   let pageViewsValue = '';
+  let priceDiscount = 0;
+
+  if(optionsToggle.checked) {
+    priceDiscount = 0.25;
+  }
 
   if(priceValue <= 8) {
     pageViewsValue = '10K';
+    priceValue = 8 - (8 * priceDiscount);
   } else if(priceValue > 8 && priceValue <= 12) {
     pageViewsValue = '50K';
+    priceValue = 12 - (12 * priceDiscount);
   } else if(priceValue > 12 && priceValue <= 16) {
     pageViewsValue = '100K';
+    priceValue = 16 - (16 * priceDiscount);
   } else if(priceValue > 16 && priceValue <= 24) {
     pageViewsValue = '500K';
+    priceValue = 24 - (24 * priceDiscount);
   } else if(priceValue > 24 && priceValue <= 36) {
     pageViewsValue = '1M';
+    priceValue = 36 - (36 * priceDiscount);
   }
 
   optionsPriceValue.textContent = `$${parseFloat(priceValue).toFixed(2)}`;
@@ -28,7 +38,7 @@ optionsRange.addEventListener('change', (event) => {
 optionsToggle.addEventListener('change', (event) => {
   let optionsRangeValue = optionsRange.value;
   if(event.target.checked) {
-    optionsRangeValue = optionsRangeValue * 0.25;
+    optionsRangeValue = optionsRangeValue - (optionsRangeValue * 0.25);
     
   } else {
     optionsRangeValue = optionsRangeValue;
