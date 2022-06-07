f2efbe73f0072273079c8c9bec0f8f3c97ac0bce
Renz Palo <renzpalo@gmail.com>
2022-06-07 16:21:59 +0800

    added script for dropdown menu

--- blogr-landing-page/js/scripts.js
+++ blogr-landing-page/js/scripts.js
@@ -1,6 +1,8 @@
 const navHamburger = document.querySelector('.nav-hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
+const dropdownLinks = document.querySelectorAll('.nav .nav-menu .dropdown-link');
+
 navHamburger.addEventListener('click', (e) => {
   if(navMenu.style.display == 'flex') {
     navMenu.style.display = 'none';
@@ -9,4 +11,22 @@ navHamburger.addEventListener('click', (e) => {
     navMenu.style.display = 'flex';
     e.currentTarget.style.backgroundImage = 'url(images/icon-close.svg)';
   }
+});
+
+dropdownLinks.forEach((dropdownLink, index) => {
+  console.log(dropdownLink, index);
+
+  dropdownLink.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const dropdownMenu = event.currentTarget.children[1];
+
+    if(dropdownMenu.style.display == 'block') {
+      dropdownMenu.style.display = 'none';
+      dropdownLink.classList.remove('active');
+    } else {
+      dropdownMenu.style.display = 'block';
+      dropdownLink.classList.add('active');
+    }
+  });
 });
