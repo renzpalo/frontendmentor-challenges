e28f17317006ca552fc64dedf8527f41ba211316
Renz Palo <renzpalo@gmail.com>
2022-06-16 18:58:40 +0800

    Generate quotes, animations, challenge completed

--- advice-generator-app/js/script.js
+++ advice-generator-app/js/script.js
@@ -4,28 +4,10 @@ const adviceGenerator = document.querySelector('.advice-generator');
 
 const adviceDiceRoll = document.getElementById('advice__dice-roll');
 
-
-// Get a quote from https://api.adviceslip.com/advice
-// const getQuote = () => {
-//   // Fetch a quote
-//   fetch('https://api.adviceslip.com/advice')
-//     .then(result => {
-//       console.log('Then:', result.json());
-
-//       return result;
-//     })
-//     // .then(data => {
-//     //   console.log('Data:', data.json());
-//     // })
-//     .catch(error => {
-//       console.log('Error:', error);
-
-//       return error;
-//     });
-// };
-
 // Await
 async function asyncGetQuote() {
+  adviceGenerator.classList.add('on-load');
+
   try {
     const result = await fetch('https://api.adviceslip.com/advice');
     const data = await result.json();
@@ -36,19 +18,23 @@ async function asyncGetQuote() {
   }
 }
 
+
+// Generate quote to DOM
 const generateQuote = (quote, isSuccess) => {
   if(isSuccess) {
     adviceId.textContent = 'Advice #' + quote.slip.id;
-    adviceQuote.textContent = quote.slip.advice;
+    adviceQuote.textContent = `\u201C${quote.slip.advice}\u201D`;
 
-    adviceGenerator.style.transition = '';
+    adviceGenerator.classList.remove('on-load');
 
-    setTimeout(() => {
-      adviceGenerator.style.transition = '.4s all ease';
-    }, 500);
+    console.log('Success Quote generated:', quote.slip.id);
   } else {
     adviceId.textContent = 'Sorry!';
     adviceQuote.textContent = 'Unable to generate advice. Please reload the page or click the dice button.';
+
+    adviceGenerator.classList.remove('on-load');
+
+    console.log('Error Quote generated:', quote.slip.id);
   }
 };
 
@@ -57,5 +43,3 @@ asyncGetQuote();
 adviceDiceRoll.addEventListener('click', () => {
   asyncGetQuote();
 });
-
-console.log(asyncGetQuote());
