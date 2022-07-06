0cf8459da892e8fd765011cb58fb9bb06547c4b2
Renz Palo <renzpalo@gmail.com>
2022-07-06 15:28:42 +0800

    tablet and desktop breakpoints stylings

---
+++ bookmark-landing-page/js/script.js
@@ -0,0 +1,34 @@
+
+const navigation = document.querySelector('.nav');
+const hamburgerMenu = document.querySelector('.nav .nav-hamburger');
+
+hamburgerMenu.addEventListener('click', (e) => {
+  navigation.classList.toggle('nav-active');
+});
+
+const faqs = [
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+];
